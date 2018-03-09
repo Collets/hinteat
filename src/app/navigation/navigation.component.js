98b4bfd8e87b1bd5e9fdcb230b3425b1e0d0a49e
DESKTOP-HJNA522\Itec <simone.coletta@scquadro.it>
2018-03-09 17:22:24 +0100

    chore: Add navigation component

---
+++ src/app/navigation/navigation.component.js
@@ -0,0 +1,35 @@
+import BaseComponent from '../base/base.component';
+import {MDCTemporaryDrawer, MDCTemporaryDrawerFoundation, util} from '@material/drawer';
+
+import './navigation.scss';
+
+/** Navigation Class */
+class NavigationComponent extends BaseComponent {
+  /**
+   * Constructor
+  */
+  constructor() {
+    super();
+
+    this._model = {
+      drawer: null,
+      menuVoices:[
+        {
+          url: '/',
+          label: 'Discover'
+        }
+      ]
+    };
+  }
+
+  /**
+   * Make an action after render of the component
+   *
+   * @memberof NavigationComponent
+   */
+  afterRender() {
+    this.model.drawer = MDCTemporaryDrawer.attachTo(document.querySelector('.mdc-drawer--temporary'));
+  }
+}
+
+export default new NavigationComponent();
